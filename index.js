15f2a10f2c79c97ee2554543290b3561fdf6a9c5
Pratik Kothare <kotharepratik129@gmail.com>
2025-10-12 00:38:58 +0530

    Onr again set up of all info  mean install ejs,express,facker package any many more

--- index.js
+++ index.js
@@ -1,9 +1,60 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+const express= require("express");
+const app= express();
+const path=require("path");
+
+
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname, "/views"));
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'sbjain',
+  password: 'NewPassword@123!'
+});
+
+
+app.get("/",(req,res)=>{
+  q='SELECT count(*) FROM users';
+try{
+  connection.query(q ,(err, result)=>{
+    if (err) throw err;
+    console.log(result[0]["count(*)"]); 
+    let value=result[0]["count(*)"]    // in output display key value pair in array, so we print 0th index then we want only value so access key 
+    res.render("home.ejs",{value});
+  });
+
+}catch(error){
+  console.log(error);
+  res.send("Some error ocured in DB!!!");
+}
+});
+
+
+let port=8080;
+app.listen(port,()=>{
+  console.log('App listing On port: ',port);
+})
+
+//Inserting new data into table
+
+
+
+
+
+
+// connection.end();
+
+
 
 let createRandomUser = () => {
   return {
     userId: faker.string.uuid(),
-    username: faker.internet.username(),
+    username: faker.internet.username(), // before version 9.1.0, use userName()
     email: faker.internet.email(),
     avatar: faker.image.avatar(),
     password: faker.internet.password(),
@@ -13,4 +64,17 @@ let createRandomUser = () => {
 }
 
 
-console.log(createRandomUser());
+// console.log(createRandomUser());
+
+// try{
+//   connection.query(q , [users], (err, result)=>{
+//     if (err) throw err;
+//     console.log(result);
+//   });
+
+// }catch(err){
+//   console.log(err);
+// }
+
+//Inserting new data into table
+
